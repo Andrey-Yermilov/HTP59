5148990632ef0b0c4364046cbb6202350d23a2b5
andrey.yermilov@gmail.com <alhimik1988>
2016-07-05 17:55:49 +0300

    3rd commit

--- src/main/java/Homework2/Task2DOP.java
+++ src/main/java/Homework2/Task2DOP.java
@@ -1,7 +1,19 @@
 package Homework2;
 
-/**
- * Created by Admin on 05.07.2016.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task2DOP {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter n");
+        int n = Integer.parseInt(br.readLine());
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (double)1/i;
+        }
+        System.out.println(sum);
+    }
 }
+
