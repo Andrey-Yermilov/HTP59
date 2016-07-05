b4b9d2c46649f00e44d551d392c74b4b7167de9a
andrey.yermilov@gmail.com <alhimik1988>
2016-07-05 18:11:14 +0300

    2nd commit

--- src/main/java/Homework2/Task4DOP.java
+++ src/main/java/Homework2/Task4DOP.java
@@ -2,6 +2,11 @@ package Homework2;
 
 public class Task4DOP {
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <=10 ; j++) {
+                System.out.printf("%4d", i*j);
+            }
+            System.out.print("\n");
+        }
     }
 }
