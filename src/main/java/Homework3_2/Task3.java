199de243ca258e2c28a165be558204df06f6d25a
andrey.yermilov@gmail.com <alhimik1988>
2016-07-07 16:45:54 +0300

    commit

--- src/main/java/Homework3_2/Task3.java
+++ src/main/java/Homework3_2/Task3.java
@@ -2,7 +2,7 @@ package Homework3_2;
 
 public class Task3 {
     public static void main(String[] args) {
-        String initialText="<i> ||English text!@#$%^ 78Abcdef //Русский текст ~~`Ёё";
+        String initialText="<i> ||English text!@#$%^ 78Abcdef //Русский текст ~~`Ё.,ё";
         String modifiedText = initialText.replaceAll("[^(A-Za-zА-Яа-яЁё\\s)]","");
         System.out.println("Original text:  "+initialText);
         System.out.println("Corrected text: "+modifiedText);
