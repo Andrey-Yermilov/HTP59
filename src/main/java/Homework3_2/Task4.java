2eedb7159a77ad1712c7925b8fe753ed86f7dabb
andrey.yermilov@gmail.com <alhimik1988>
2016-07-07 18:09:42 +0300

    commit

--- src/main/java/Homework3_2/Task4.java
+++ src/main/java/Homework3_2/Task4.java
@@ -2,7 +2,19 @@ package Homework3_2;
 
 public class Task4 {
     public static void main(String[] args) {
-    String initialString="Returns a new string resulting from replacing all occurrences of oldChar in this string with newChar abcd erty qwer";
+        String initialString="Returns a new string resulting from replacing all occurrences of oldChar in this string with newChar abcd erty qwer";
         int n = 4; // length of words which should be deleted
+        String correctedString = initialString.toLowerCase();
+        String[] words = correctedString.split("[\\s]");
+        for (int i = 0; i < words.length; i++)
+            if (words[i].length()==n){
+                if (!(words[i].startsWith("a")||words[i].startsWith("e")||words[i].startsWith("i")||words[i].startsWith("o")||words[i].startsWith("u")||words[i].startsWith("y"))){
+                    words[i]="";
+                }
+            }
+        for (String word:words
+             ) {
+            System.out.print((word+" ").replaceAll("",""));
+        }
     }
 }
