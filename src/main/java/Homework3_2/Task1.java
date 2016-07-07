43db4b4a828c79ba74977ce52b7e1514eda4fb63
andrey.yermilov@gmail.com <alhimik1988>
2016-07-07 14:03:43 +0300

    commit

---
+++ src/main/java/Homework3_2/Task1.java
@@ -0,0 +1,34 @@
+package Homework3_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * method replaces symbol in k=2 position with entered symbol
+ */
+public class Task1 {
+    public static void main(String[] args) throws IOException{
+        String initialString="Returns a new string resulting from replacing all occurrences of oldChar in this string with newChar";
+        int k=2;
+        String finalString="";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter symbol");
+        String symbol =  br.readLine();
+        String[] words = initialString.split("[\\s]");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length()>=k){
+                words[i]=words[i].substring(0,k-1)+symbol+words[i].substring(k, words[i].length());
+            }
+        }
+
+        for (String s:words) {
+            finalString+=" "+s;
+            finalString=finalString.trim();
+        }
+
+        System.out.println("String1: "+initialString);
+        System.out.println("String2: "+finalString);
+
+    }
+}
