c5e52472b919ef7e810cb14b2e40439fe039b1fa
andrey.yermilov@gmail.com <alhimik1988>
2016-07-07 15:03:04 +0300

    commit

---
+++ src/main/java/Homework3_2/Task2.java
@@ -0,0 +1,17 @@
+package Homework3_2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String initialString="Returns a new string resulting from replacing all occurrences of oldChar in this string with newChar";
+        initialString = initialString.replaceAll(" ","");
+        String modifiedString = initialString.toUpperCase();
+        for (int i = 0; i < initialString.length(); i++) {
+            System.out.print(initialString.charAt(i)+"  ");
+        }
+        System.out.println();
+        for (int i = 0; i < modifiedString.length(); i++) {
+            Character c = modifiedString.charAt(i);
+            System.out.printf("%02d ",(c.hashCode()-64));
+        }
+    }
+}
