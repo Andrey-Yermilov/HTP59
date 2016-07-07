196a13504c2e624cae46bf545b2a36e72aa0de67
andrey.yermilov@gmail.com <alhimik1988>
2016-07-07 17:53:10 +0300

    commit

---
+++ src/main/java/Homework3_2/Task4.java
@@ -0,0 +1,8 @@
+package Homework3_2;
+
+public class Task4 {
+    public static void main(String[] args) {
+    String initialString="Returns a new string resulting from replacing all occurrences of oldChar in this string with newChar abcd erty qwer";
+        int n = 4; // length of words which should be deleted
+    }
+}
