cbe3da44f7039ef22c927311f1ee433785efe200
andrey.yermilov@gmail.com <alhimik1988>
2016-07-11 17:51:17 +0300

    commit

---
+++ src/main/java/Homework3/Task5.java
@@ -0,0 +1,27 @@
+package Homework3;
+
+import java.util.Random;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int arr[] = new int[15];
+        Random rn = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rn.nextInt(100) + 1;
+            System.out.print(arr[i] + " | ");
+        }
+        int min=arr[0];
+        int n = 0;
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+                n=i;
+            }
+        }
+        System.out.println();
+        System.out.println(String.format("min value = %d", min));
+        System.out.print(String.format("first index = %d",n));
+
+    }
+}
+
