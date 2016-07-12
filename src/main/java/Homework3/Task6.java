6752859f432b9c02f61894f2bb398de149f72bb6
andrey.yermilov@gmail.com <alhimik1988>
2016-07-12 17:24:37 +0300

    commit

---
+++ src/main/java/Homework3/Task6.java
@@ -0,0 +1,23 @@
+package Homework3;
+
+import java.util.Random;
+
+public class Task6{
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        Random rn = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rn.nextInt(201) - 100;
+            System.out.print(arr[i] + " | ");
+        }
+       int numberOfNegatives = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] < 0) {
+                numberOfNegatives++;
+            }
+        }
+        System.out.println();
+        System.out.println(String.format("number of negative elements = %d", numberOfNegatives));
+
+    }
+}
