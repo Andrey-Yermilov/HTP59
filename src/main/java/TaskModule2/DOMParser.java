a03861f75b191539fe721fb6f4038c4d33fa3cb8
andrey.yermilov@gmail.com <alhimik1988>
2016-08-15 21:06:39 +0300

    Module 2 task

---
+++ src/main/java/TaskModule2/DOMParser.java
@@ -0,0 +1,21 @@
+package TaskModule2;
+
+import TaskModule2.generated.Flowers;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DOMParser {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(Flowers.class);
+        Unmarshaller u = jc.createUnmarshaller();
+        FileReader reader = new FileReader("src/main/java/TaskModule2/plants.xml");
+        Flowers flowers = (Flowers) u.unmarshal(reader);
+        System.out.println(flowers);
+    }
+}
